ef58e39b7d1d16b8e5e2aeb548d779e26ab76e31
Jayce <maihoang171@gmail.com>
2025-12-07 11:45:00 +0700

    first init

---
+++ src/services/axios.ts
@@ -0,0 +1,48 @@
+import axios from "axios";
+import type { AxiosRequestConfig } from "axios";
+import { camelizeKeys, decamelizeKeys } from "humps";
+
+export function toArray<T>(t?: T | T[]): T[] {
+  return Array.isArray(t) ? t : t ? [t] : [];
+}
+
+const baseURL = "http://localhost:8080/api/v1";
+const defaultConfig: AxiosRequestConfig = {
+  timeout: 60000,
+  withCredentials: true,
+  baseURL,
+};
+
+const decamelizeRequestKeys = (data: unknown) => {
+  return data instanceof FormData ? data : decamelizeKeys(data);
+};
+
+const camelizeResponseKeys = (data: unknown) => {
+  return data instanceof Blob ? data : camelizeKeys(data);
+};
+
+const createAxiosClient = (config: AxiosRequestConfig = defaultConfig) => {
+  const instance = axios.create({
+    ...config,
+    transformResponse: [
+      ...toArray(axios.defaults.transformResponse),
+      camelizeResponseKeys,
+    ],
+    transformRequest: [
+      decamelizeRequestKeys,
+      ...toArray(axios.defaults.transformRequest),
+    ],
+  });
+
+  instance.interceptors.request.use((config) => {
+    const token = localStorage.getItem("token");
+    if (token && config.headers) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  });
+
+  return instance;
+};
+
+export const axiosClient = createAxiosClient();
